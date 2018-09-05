9d44c635784f2a4ad431d8b8ca8d1018ba2b2606
Raushan <raushan790@gmail.com>
2018-09-05 21:20:12 +0530

    new files

---
+++ StudentExpatAdmin/Models/Agreements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace studentexpat.com.Models
+{
+    public partial class Agreements
+    {
+        public int Id { get; set; }
+        public int? SchoolId { get; set; }
+        public string Agreement { get; set; }
+        public DateTime? SignedOn { get; set; }
+        public string Attachment { get; set; }
+        public string Warnings { get; set; }
+        public string Comments { get; set; }
+        public DateTime? FollowUp { get; set; }
+
+        public Schools School { get; set; }
+    }
+}
